212251fdc692de295ffd2ec2c989c4468878ee7e
sarath <tracesarath@gmail.com>
2017-05-01 21:45:11 -0500

    update

---
+++ Source/MongoRestApplication/Server.js
@@ -0,0 +1,53 @@
+var express=require('express');
+var nodemailer = require("nodemailer");
+var app=express();
+
+/*
+	Here we are configuring our SMTP Server details.
+	STMP is mail server which is responsible for sending and recieving email.
+*/
+
+var smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: 'pinatme@gmail.com',
+        pass: 'cloudera34'
+    },
+    tls: {rejectUnauthorized: false},
+    debug:true
+});
+
+
+/*------------------SMTP Over-----------------------------*/
+
+/*------------------Routing Started ------------------------*/
+
+app.get('/',function(req,res){
+	res.sendfile('BookAppointment1.html');
+});
+app.get('/send',function(req,res){
+	var mailOptions={
+		to : req.query.to,
+		subject : req.query.subject,
+		text: req.query.text
+
+	}
+	console.log(mailOptions);
+	smtpTransport.sendMail(mailOptions, function(error, response){
+   	 if(error){
+        	console.log(error);
+		res.end("error");
+	 }else{
+        	console.log("Message sent: " + response.message);
+		res.end("sent");
+    	 }
+});
+});
+
+/*--------------------Routing Over----------------------------*/
+
+app.listen(4000,function(){
+	console.log("Express Started on Port 4000");
+});
